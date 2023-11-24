8d5382f5467422c7dfaafb23460e1bcda8a653d3
Adrian Fonseca <adrian2020fm@gmail.com>
2023-11-24 11:44:16 -0500

    first commit

---
+++ Apuntes.js
@@ -0,0 +1,36 @@
+//esto es un comentario
+
+/*
+    esto tambien es un comentario  
+*/
+
+alert('Hola!');
+
+// VARIABLES
+// Sumando dos numeros 
+
+var pi = 3.141512; 
+var suma = 1 + 2;
+
+
+
+var numero1 = 1;
+var numero2 = 2; 
+
+//preguntamos al usuario numero1
+var numero1 = 1;
+
+//preguntamos al usuario numero2
+var numero2 = 2;
+
+//
+suma = numero1 + numero2;
+var resta = numero1 - numero2;
+var multiplicacion = numero1 * numero2;
+var divicion = numero1 / numero2;
+var residuo = numero1 % numero2;
+
+//var $number;
+//var _number;
+//var $otherNumber;
+//var $_a_asd;
